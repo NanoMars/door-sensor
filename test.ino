95a07f4dc52377c756bbbff4f23813215de150f5
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-20 13:18:23 +1000

    idk

---
+++ test.ino
@@ -0,0 +1,42 @@
+#include <Ticker.h>
+
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 13
+#endif
+/*
+Ticker flipper;
+
+
+int count = 0;
+
+void flip() {
+  int state = digitalRead(LED_BUILTIN);  // get the current state of GPIO1 pin
+  digitalWrite(LED_BUILTIN, !state);     // set pin to the opposite state
+
+  ++count;
+  // when the counter reaches a certain value, start blinking like crazy
+  if (count == 20) {
+    flipper.attach(0.1, flip);
+  }
+  // when the counter reaches yet another value, stop blinking
+  else if (count == 120) {
+    flipper.detach();
+  }
+}*/
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
+  // flip the pin every 0.3s
+  //flipper.attach(0.3, flip);
+  //Serial.begin(115200);
+}
+
+void loop() {
+
+  digitalWrite (LED_BUILTIN, HIGH);    // turn on the LED
+  delay(500);    // wait for half a second or 500 milliseconds
+  digitalWrite (LED_BUILTIN, LOW);    // turn off the LED
+  delay(500);    // wait for half a second or 500 milliseconds
+}
