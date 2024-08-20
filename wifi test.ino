d80f555deb7232d9cfe6d29273c20a56557a190a
NanoMars <Armand.packham@gmail.com>
2024-08-20 17:50:58 +1000

    Trying to get gsheets to work

--- wifi test.ino
+++ wifi test.ino
@@ -8,20 +8,20 @@
 #define WIFI_PASSWORD "littleFish"
 
 void setup() {
-    /*Serial.begin(115200);
+    Serial.begin(115200);
     WiFi.setAutoReconnect(true);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);*/
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 
     Serial.print("Connecting to Wi-Fi");
-    /*while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED) {
       Serial.print(".");
       delay(1000);
     }
-    */
+    
     Serial.println();
     Serial.print("Connected with IP: ");
     
-    //Serial.println(WiFi.localIP());
+    Serial.println(WiFi.localIP());
     Serial.println();
 }
 
