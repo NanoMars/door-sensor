8a58fe050bd643c53eb15d439f1c986bcd01d592
NanoMars <Armand.packham@gmail.com>
2024-08-21 11:03:06 +1000

    slowly adding code in to see what's causing these issues

--- wifi test.ino
+++ wifi test.ino
@@ -3,9 +3,22 @@
 #include "time.h"
 #include <ESP_Google_Sheet_Client.h>
 
+#include <GS_SDHelper.h>
+#include <FirebaseJson.h>
 
-#define WIFI_SSID "Armand's_Wifi"
-#define WIFI_PASSWORD "littleFish"
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+#define PROJECT_ID ""
+#define CLIENT_EMAIL ""
+const char PRIVATE_KEY[] PROGMEM = "-----BEGIN PRIVATE KEY-----\b\n-----END PRIVATE KEY-----\n";
+const char spreadsheetId[] = "";
+
+// Token Callback function
+void tokenStatusCallback(TokenInfo info);
+
+// NTP server to request epoch time
+//const char* ntpServer = "pool.ntp.org";
 
 void setup() {
     Serial.begin(115200);
@@ -23,6 +36,18 @@ void setup() {
     
     Serial.println(WiFi.localIP());
     Serial.println();
+
+
+    GSheet.printf("ESP Google Sheet Client v%s\n\n", ESP_GOOGLE_SHEET_CLIENT_VERSION);
+
+    // Set the callback for Google API access token generation status (for debug only)
+    GSheet.setTokenCallback(tokenStatusCallback);
+
+    // Set the seconds to refresh the auth token before expire (60 to 3540, default is 300 seconds)
+    GSheet.setPrerefreshSeconds(10 * 60);
+
+    // Begin the access token generation for Google API authentication
+    GSheet.begin(CLIENT_EMAIL, PROJECT_ID, PRIVATE_KEY);
 }
 
 
